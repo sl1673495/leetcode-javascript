b74f37728713582c499bb7180939b27811972065
ssh <949589855@qq.com>
2020-03-16 17:22:39 +0800

    feat: 最简单的promise异步调用链

--- promise-easy.js
+++ promise-easy.js
@@ -1,21 +1,13 @@
 /**
  * 仅实现异步链式调用的简化版
  */
-
 function Promise(excutor) {
   var self = this
-  self.status = "pending" // Promise当前的状态
-  self.data = undefined // Promise的值
   self.onResolvedCallback = [] // Promise resolve时的回调函数集，因为在Promise结束之前有可能有多个回调添加到它上面
 
   function resolve(value) {
-    setTimeout(() => {
-      if (self.status === "pending") {
-        self.status = "resolved"
-        self.data = value
-        self.onResolvedCallback.forEach(callback => callback(value))
-      }
-    })
+    self.data = value
+    self.onResolvedCallback.forEach(callback => callback(value))
   }
 
   excutor(resolve.bind(self))
@@ -46,14 +38,6 @@ new Promise(resolve => {
     return new Promise(resolve => {
       setTimeout(() => {
         resolve(2)
-      }, 500)
-    })
-  })
-  .then(res => {
-    console.log(res)
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(3)
       }, 500)
     })
   })
