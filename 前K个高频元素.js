f4202206c07fbefbf77845e66a78646142a63cbd
ssh <949589855@qq.com>
2020-04-03 14:45:36 +0800

    feat: 前K个高频元素

---
+++ 前K个高频元素.js
@@ -0,0 +1,38 @@
+/**
+ * 
+  347. 前 K 个高频元素
+  给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+
+  示例 1:
+
+  输入: nums = [1,1,1,2,2,3], k = 2
+  输出: [1,2]
+  示例 2:
+
+  输入: nums = [1], k = 1
+  输出: [1]
+  说明：
+
+  你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+  你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+ */
+
+var topKFrequent = function(nums, k) {
+  var map = new Map()
+  for (var i = 0; i < nums.length; i++) {
+    var num = nums[i]
+    var count = map.get(num)
+    if (!count) {
+      map.set(num, 1)
+    } else {
+      map.set(num, count + 1)
+    }
+  }
+  var sorted = Array.from(map.entries()).sort((a, b) => b[1] - a[1])
+  var result = []
+  for (var i = 0; i < k; i++) {
+    result.push(sorted[i][0])
+  }
+  return result
+}
+console.log(topKFrequent([1, 2, 3, 3, 3, 4, 4, 4, 4, 5, 1, 2], 2))
