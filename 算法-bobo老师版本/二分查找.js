4df3be6ef2ffb3206df857a93c21ab3b128a7544
ssh <949589855@qq.com>
2020-04-28 01:53:06 +0800

    feat: 二分查找-bobo

---
+++ 算法-bobo老师版本/二分查找.js
@@ -0,0 +1,21 @@
+function binarySearch(arr, n, target) {
+  // 在[l...r]的范围里寻找target
+  let l = 0;
+  let r = n - 1;
+  while (l <= r) {
+    let mid = Math.round((l + r) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    }
+
+    if (target > arr[mid]) {
+      l = mid + 1; // 在[mid+1...r]的范围里寻找target
+    } else {
+      r = mid - 1;
+    }
+  }
+  return -1;
+}
+
+console.log(console.log(binarySearch([1, 2, 3, 4, 5], 5, 2)));
