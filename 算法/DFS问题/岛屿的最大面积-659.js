6a327b25deb5f9b2fa4dcc213e4885006b4a9efc
ssh <949589855@qq.com>
2020-04-29 00:33:39 +0800

    feat: 岛屿最大面积 调整代码结构

---
+++ 算法/DFS问题/岛屿的最大面积-659.js
@@ -0,0 +1,41 @@
+// https://github.com/sl1673495/daily-plan/issues/18
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function (grid) {
+  let yLen = grid.length;
+  if (!yLen) return grid;
+  let xLen = grid[0].length;
+  let max = 0;
+
+  for (let y = 0; y < yLen; y++) {
+    for (let x = 0; x < xLen; x++) {
+      if (grid[y][x] === 1) {
+        let countRef = { current: 0 };
+        dfs(grid, y, x, countRef);
+        if (countRef.current > max) {
+          max = countRef.current;
+        }
+      }
+    }
+  }
+  return max;
+};
+
+function dfs(grid, y, x, countRef) {
+  if (!grid[y] || !grid[y][x] || grid[y][x] === 0 || grid[y][x] === "COMPLETE") {
+    return;
+  }
+
+  if (grid[y][x] === 1) {
+    grid[y][x] = "COMPLETE";
+    countRef.current++;
+  }
+
+  dfs(grid, y - 1, x, countRef);
+  dfs(grid, y + 1, x, countRef);
+  dfs(grid, y, x - 1, countRef);
+  dfs(grid, y, x + 1, countRef);
+}
