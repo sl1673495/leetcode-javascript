3f0b7e2ea7140638fd0e47d755c9052ebb1fbb7c
ssh <949589855@qq.com>
2020-04-30 11:52:54 +0800

    feat: 树行中找最大值

---
+++ 算法/BFS问题/在每个树行中找最大值-515.js
@@ -0,0 +1,93 @@
+/**
+ * 您需要在二叉树的每一行中找到最大的值。
+
+  示例：
+
+  输入: 
+
+            5
+           / \
+          14   2
+         / \    \  
+        1   3    9 
+
+  输出: [1, 3, 9]
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var largestValues = function (root) {
+  if (!root) return [];
+  let queue = [root];
+  let maximums = [];
+
+  while (queue.length) {
+    let max = Number.MIN_SAFE_INTEGER;
+    // 这里需要先缓存length 这个length代表当前层级的所有节点
+    // 在循环开始后 会push新的节点 length就不稳定了
+    let len = queue.length;
+    for (let i = 0; i < len; i++) {
+      let node = queue[i];
+      max = Math.max(node.val, max);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    // 本「层级」处理完毕，截取掉。
+    queue.splice(0, len);
+
+    // 这个for循环结束后 代表当前层级的节点全部处理完毕
+    // 直接把计算出来的最大值push到数组里即可。
+    maximums.push(max);
+  }
+
+  return maximums;
+};
+
+/** test case **/
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+/** 预期：[1, 3, 9] */
+var root = new TreeNode(1);
+root.left = new TreeNode(3);
+root.right = new TreeNode(2);
+root.left.left = new TreeNode(5);
+root.left.right = new TreeNode(3);
+root.right.right = new TreeNode(9);
+
+/**
+       5
+      /
+    14
+   /
+  1
+
+ */
+/** 预期：[5, 14, 1] */
+var root2 = new TreeNode(5);
+root2.left = new TreeNode(14);
+root2.left.left = new TreeNode(1);
+
+console.log(largestValues(root));
+console.log(largestValues(root2));
---
+++ 算法/DFS问题/图像渲染-733.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @return {number[][]}
+ */
+var floodFill = function (image, sr, sc, newColor) {
+  let current = image[sr][sc];
+  dfs(image, sr, sc, current, newColor);
+  return image;
+};
+
+function dfs(image, sr, sc, current, newColor) {
+  let row = image[sr];
+  if (row === undefined) return;
+
+  let cell = row[sc];
+  if (cell === undefined || cell !== current || cell === newColor) return;
+
+  row[sc] = newColor;
+  dfs(image, sr + 1, sc, current, newColor);
+  dfs(image, sr - 1, sc, current, newColor);
+  dfs(image, sr, sc + 1, current, newColor);
+  dfs(image, sr, sc - 1, current, newColor);
+}
+
+console.log(
+  floodFill(
+    [
+      [0, 0, 0],
+      [0, 0, 0],
+    ],
+    0,
+    0,
+    2
+  )
+);
