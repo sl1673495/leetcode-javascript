4b6c50557c5ee9a9232ef889a0b4c05f25aaddca
ssh <949589855@qq.com>
2020-05-01 13:20:30 +0800

    feat: 找硬币-动态规划版

---
+++ 算法/动态规划/找硬币-记忆递归版.js
@@ -0,0 +1,28 @@
+function f(n) {
+  function makeChange(amount) {
+      if(amount <= 0) return 0
+  
+      // 校验是否已经在备忘录中存在结果,如果存在返回即可
+      if(cache[amount]) return cache[amount]
+  
+      let min = Infinity
+      if (amount >= 1) {
+          min = Math.min(makeChange(amount-1) + 1, min)
+      }
+  
+      if (amount >= 5) {
+          min = Math.min(makeChange(amount-5) + 1, min)
+      }
+  
+      if (amount >= 11) {
+          min = Math.min(makeChange(amount-11) + 1, min)
+      }
+  
+      return (cache[amount] = min)
+  }
+  // 备忘录
+  const cache = []
+  return makeChange(n)
+}
+
+console.log(f(705))
