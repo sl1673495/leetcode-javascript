378a297aff0a81d9dcea2c51fba3400769a317fc
ssh <949589855@qq.com>
2020-05-04 11:00:02 +0800

    feat: 打家劫舍|||

--- 动态规划/打家劫舍III-337.js
+++ 动态规划/打家劫舍III-337.js
@@ -19,74 +19,27 @@ const TreeNode = require("../工具/二叉树");
  * @param {TreeNode} root
  * @return {number}
  */
+let memo = new WeakMap()
 let rob = function (root) {
-  if (!root) return 0;
-
-  // 先求出树的层级 以及每层对应的所有节点
-  const { maxLevel, levelNodesMap } = makeLevelNodesMap(root);
-
-  let getLevelValue = (level) => {
-    const nodes = levelNodesMap.get(level);
-    if (!nodes) {
-      return 0;
-    }
-    let value = 0;
-    for (let i = 0; i < nodes.length; i++) {
-      value += nodes[i].val;
-    }
-    return value;
-  };
-
-  // 接下来开始进入打劫时间
-  let dp = [];
-  for (let i = maxLevel; i >= 0; i--) {
-    let max = 0;
-    // 每次需要跳过一个层级，不能打劫相邻的房子。
-    for (j = i; j <= maxLevel; j++) {
-      // 当前层级的价值加上跳一层后为起点的打劫的最大价值
-      max = Math.max(max, getLevelValue(j) + (dp[j + 2] || 0));
-    }
-    dp[i] = max;
+  if (!root) {
+    return 0;
   }
 
-  return dp[0];
+  let memorized = memo.get(root)
+  if (memorized) {
+    return memorized
+  }
+
+  let notRob = rob(root.left) + rob(root.right);
+  let robNow =
+    (root.val || 0) +
+    (root.left ? rob(root.left.left) + rob(root.left.right) : 0) +
+    (root.right ? rob(root.right.left) + rob(root.right.right) : 0);
+
+  let max = Math.max(notRob, robNow);
+  memo.set(root, max)
+  return max;
 };
-
-function makeLevelNodesMap(root) {
-  let queue = [root];
-  // 每一层级的节点记录下来
-  let levelNodesMap = new Map();
-  let maxLevel = 0;
-  while (queue.length) {
-    // 缓存当前层级的节点长度
-    let levelLen = queue.length;
-    let levelNodes = [];
-    for (let i = 0; i < levelLen; i++) {
-      let node = queue[i];
-      levelNodes.push(node);
-
-      // 这里是下一层级了 先放入队列中
-      if (node.left) {
-        queue.push(node.left);
-      }
-      if (node.right) {
-        queue.push(node.right);
-      }
-    }
-
-    // 本层处理完毕 移除出数组
-    for (let i = 0; i < levelLen; i++) {
-      queue.shift();
-    }
-
-    levelNodesMap.set(maxLevel, levelNodes);
-    if (queue.length) {
-      maxLevel++;
-    }
-  }
-
-  return { maxLevel, levelNodesMap };
-}
 
 // 这种情况是不对的 应该计算出7
 let tree1 = new TreeNode(2);
