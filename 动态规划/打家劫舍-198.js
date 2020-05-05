51fc39733b04314ad4e7bc40637320b3439a1fec
ssh <949589855@qq.com>
2020-05-05 12:08:22 +0800

    feat: 打家劫舍第二版

--- 动态规划/打家劫舍-198.js
+++ 动态规划/打家劫舍-198.js
@@ -34,6 +34,39 @@ assert(robRecurision([1, 2, 3, 1]) === 4);
 assert(robRecurision([2, 7, 9, 3, 1]) === 12);
 
 /**
+ * 打家劫舍-递归版2
+ * 把打劫细分为
+ *
+ * 1. 打劫当前房子 那么下次就要从start + 2开始
+ * 2. 打劫下一个房子 那么就直接从start + 1开始
+ *
+ * 求这两者间的最大值
+ * @param {*} nums
+ */
+let robRecurision2 = function (nums) {
+  let memo = [];
+  function tryRob(nums, start) {
+    let memorized = memo[start];
+    if (memorized) return memorized;
+
+    if (start > nums.length - 1) return 0;
+
+    let robNow = nums[start] + tryRob(nums, start + 2);
+    let robNext = tryRob(nums, start + 1);
+
+    let best = Math.max(robNext, robNow);
+    memo[start] = best;
+
+    return best;
+  }
+
+  return tryRob(nums, 0);
+};
+
+assert(robRecurision2([1, 2, 3, 1]) === 4);
+assert(robRecurision2([2, 7, 9, 3, 1]) === 12);
+
+/**
  * 打家劫舍 DP版
  * @param {number[]} nums
  * @return {number}
@@ -58,3 +91,27 @@ let rob = function (nums) {
 
 assert(rob([1, 2, 3, 1]) === 4);
 assert(rob([2, 7, 9, 3, 1]) === 12);
+
+/**
+ * 打家劫舍 DP版2
+ * @param {number[]} nums
+ * @return {number}
+ */
+let rob2 = function (nums) {
+  if (!nums.length) {
+    return 0;
+  }
+  let dp = [];
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    let robNow = nums[i] + (dp[i + 2] || 0)
+    let robNext = dp[i + 1] || 0
+
+    dp[i] = Math.max(robNow, robNext)
+  }
+
+  return dp[0];
+};
+
+
+console.log(rob2([1, 10, 3, 1, 5]));
