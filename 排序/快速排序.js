021b605fe5ba56396777c3fa496d40b1274ec6aa
sl1673495 <949589855@qq.com>
2020-05-22 02:18:47 +0800

    feat: 快速排序

---
+++ 排序/快速排序.js
@@ -0,0 +1,46 @@
+const swap = require("../工具/交换");
+
+/**
+ *
+ * @param {number[]} arr
+ */
+function quickSort(arr) {
+  _quickSort(arr, 0, n - 1);
+}
+
+/**
+ * 对 arr[l...r] 部分进行快速排序
+ * @param {number[]} arr
+ * @param {number} l 左边界
+ * @param {number} r 右边界
+ */
+function _quickSort(arr, l, r) {
+  if (l >= r) {
+    return;
+  }
+  let p = partition(arr, l, r);
+  __quickSort(arr, l, p - 1);
+  __quickSort(arr, p + 1, r);
+}
+
+/**
+ * 对 arr[l...r] 部分进行快速排序
+ * @param {number[]} arr
+ * @param {number} l 左边界
+ * @param {number} r 右边界
+ * @returns {number} 返回索引值p，使得arr[l...p-1] < arr[p] < arr[p+1...r]
+ */
+function partition(arr, l, r) {
+  // 取一个基准值 取第一项
+  let v = arr[l];
+
+  // arr[l+1...j] < v; arr[j+1...i) > v
+  let j = l;
+  for (let i = l + 1; i <= r; i++) {
+    if (arr[i] < v) {
+      // 如果当前值小于基准值的话，就交换到j + 1后的位置去。
+      swap(arr, j + 1, i);
+      j++;
+    }
+  }
+}
