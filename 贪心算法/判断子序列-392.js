c9ddba8a422ad45953a6195c6926c5a46ec36cdc
sl1673495 <949589855@qq.com>
2020-06-21 01:18:36 +0800

    refactor(判断子序列): change i position

--- 贪心算法/判断子序列-392.js
+++ 贪心算法/判断子序列-392.js
@@ -23,3 +23,28 @@ let isSubsequence = function (s, t) {
 
   return false
 }
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+let isSubsequence = function (s, t) {
+  let sl = s.length
+  if (!sl) {
+    return true
+  }
+
+  let i = 0
+  for (let j = 0; j < t.length; j++) {
+    let target = s[i]
+    let cur = t[j]
+    if (cur === target) {
+      i++
+      if (i === sl) {
+        return true
+      }
+    }
+  }
+
+  return false
+}
