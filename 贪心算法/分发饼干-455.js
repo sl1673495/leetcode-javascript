1cc56fa3686bb86b10e955e0bda54a6a9d348862
sl1673495 <949589855@qq.com>
2020-06-20 22:55:03 +0800

    feat: 分发饼干

---
+++ 贪心算法/分发饼干-455.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function (g, s) {
+  g.sort((a, b) => a - b)
+  s.sort((a, b) => a - b)
+
+  let i = 0
+  let j = 0
+
+  let count = 0
+  while (j < s.length && i < g.length) {
+    let need = g[i]
+    let cookie = s[j]
+
+    if (cookie >= need) {
+      count++
+      i++
+      j++
+    } else {
+      j++
+    }
+  }
+
+  return count
+}
